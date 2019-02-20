42f41308eed2d2597eff8c0b4bd320f57fcf80e3
imdhemy <m.dhemy@outlook.com>
2019-02-20 20:36:04 +0200

    User search as a local scope.

--- src/Traits/SearchTrait.php
+++ src/Traits/SearchTrait.php
@@ -1,19 +1,72 @@
 <?php
 namespace Imdhemy\Berrylastic\Traits;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
+use ONGR\ElasticsearchDSL\BuilderInterface;
 use ONGR\ElasticsearchDSL\Search;
 
 trait SearchTrait
 {
+    /**
+     * Supported search queries
+     *
+     * @var array
+     */
     protected $supported_queries = [
         'match',
         'multiMatch',
         'matchPhrase'
     ];
 
+    /**
+     * Hold a collection of search queries
+     *
+     * @var Illuminate\Support\Collection
+     */
     protected $search_queries;
+
+    /**
+     * Hold raw search results
+     *
+     * @var array
+     */
+    protected $raw_search_results;
+
+    /**
+     * Hold elastic search hits
+     *
+     * @var Illuminate\Support\Collection
+     */
+    protected $hits;
+
+    /**
+     * Restrict query to search results
+     *
+     * @param  Builder $builder
+     * @return Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeSearch(Builder $builder, ?string $primary_key = null)
+    {
+        $params = $this->getSearchParams();
+        $results = $this->client()->search($params);
+        $this->setSearchResults($results);
+        return $this->scopedSearchQuery($builder, $primary_key);
+    }
+
+    /**
+     * Get elasticsearch hits
+     *
+     * @return Illuminate\Support\Collection
+     */
+    public function hits() : Collection
+    {
+        if (null === $this->hits) {
+            $this->hits = new Collection();
+        }
+        return $this->hits;
+    }
 
     /**
      * Handle dynamic method calls into the model.
@@ -25,32 +78,61 @@ trait SearchTrait
     public function __call($method, $parameters)
     {
         if (in_array($method, $this->supported_queries)) {
-            return $this->appendSearchQuery($method, $parameters);
+            return $this->appendDslQuery($this->getDslQuery($method, $parameters));
         }
         return parent::__call($method, $parameters);
     }
 
-    public function search()
+    /**
+     * Get instance of ElasricsearchDSL Fulltext Query
+     *
+     * @param  string $method
+     * @param  array $parameters
+     * @return  ONGR\ElasticsearchDSL\BuilderInterface
+     */
+    protected function getDslQuery($method, $parameters) : BuilderInterface
     {
-        $params = $this->getSearchParams();
-        $results = $this->client()->search($params);
-        $hits = new Collection($results['hits']['hits']);
-
-        $identifiers = $hits->pluck('_id');
-        $primary_key = $this->getKeyName();
-        $query = $this->whereIn($primary_key, $identifiers);
-
-        if ($identifiers->count()) {
-            $ordered_identifiers = implode(',', $identifiers->toArray());
-            $query = $query->orderByRaw(\DB::raw("FIELD($primary_key, $ordered_identifiers)"));
-        }
-        return $query;
+        $class_name = ucfirst(Str::camel("{$method}Query"));
+        $class = "\ONGR\ElasticsearchDSL\Query\FullText\\{$class_name}";
+        return new $class(...$parameters);
     }
 
-    protected function getSearchParams()
+    /**
+     * Append a DSL query to search_queries list
+     *
+     * @param  BuilderInterface $query
+     * @return
+     */
+    protected function appendDslQuery(BuilderInterface $query)
+    {
+        $search_queries = $this->searchQueries();
+        $search_queries->push($query);
+        $this->search_queries = $search_queries;
+        return $this;
+    }
+
+    /**
+     * Get the collection of search queries
+     *
+     * @return Illuminate\Support\Collection
+     */
+    protected function searchQueries() : Collection
+    {
+        if (null === $this->search_queries) {
+            $this->search_queries = new Collection();
+        }
+        return $this->search_queries;
+    }
+
+    /**
+     * Get search params array
+     *
+     * @return array
+     */
+    protected function getSearchParams() : array
     {
         $search = new Search();
-        foreach ($this->search_queries as $query) {
+        foreach ($this->searchQueries() as $query) {
             $search->addQuery($query);
         }
         return [
@@ -60,28 +142,32 @@ trait SearchTrait
         ];
     }
 
-    protected function getDslQuery($method, $parameters)
+    /**
+     * Set raw search results data
+     *
+     * @param array $results
+     */
+    protected function setSearchResults(array $results) : void
     {
-        $class_name = ucfirst(Str::camel("{$method}Query"));
-        $class = "\ONGR\ElasticsearchDSL\Query\FullText\\{$class_name}";
-        $query = new $class(...$parameters);
-        return $query;
+        $this->raw_search_results = $results;
+        $this->hits = new Collection($results['hits']['hits']);
     }
 
-    protected function appendSearchQuery($method, $parameters)
+    /**
+     * Restrict query builder to search results
+     *
+     * @param  Builder $builder
+     * @return Illuminate\Database\Eloquent\Builder
+     */
+    protected function scopedSearchQuery(Builder $builder, ?string $primary_key = null) : Builder
     {
-        $query = $this->getDslQuery($method, $parameters);
-        $search_queries = $this->searchQueries();
-        $search_queries->push($query);
-        $this->search_queries = $search_queries;
-        return $this;
-    }
-
-    protected function searchQueries()
-    {
-        if (null === $this->search_queries) {
-            $this->search_queries = new Collection();
+        $identifiers = $this->hits()->pluck('_id');
+        $primary_key = $primary_key ?: $this->getKeyName();
+        $builder = $builder->whereIn($primary_key, $identifiers);
+        if ($identifiers->count()) {
+            $ordered_identifiers = $identifiers->implode(',');
+            $builder = $builder->orderByRaw(\DB::raw("FIELD($primary_key, $ordered_identifiers)"));
         }
-        return $this->search_queries;
+        return $builder;
     }
 }
